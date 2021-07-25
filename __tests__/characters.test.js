93bc3869e1514c9c9537a8a41568083a16ec6ac4
Andre Velasquez <andrethefirst21@gmail.com>
2021-07-25 11:38:25 -0700

    pass knight selection test

--- __tests__/characters.test.js
+++ __tests__/characters.test.js
@@ -17,4 +17,13 @@ describe('Character', () => {
     expect(character.magic).toEqual(90);
     expect(character.defense).toEqual(20);
   });
+  test('should correctly fill in the character type and stats to match for knight', () => { 
+    character.knight()
+    expect(character.type).toEqual("knight");
+    expect(character.strength).toEqual(75);
+    expect(character.intelligence).toEqual(30);
+    expect(character.speed).toEqual(50);
+    expect(character.magic).toEqual(10);
+    expect(character.defense).toEqual(65);
+  });
 });
